38b4eac2bc562b6bb954795e313ece745547f960
Sirojiddin96 <herodot.sk@mail.ru>
2021-10-20 16:31:39 +0900

    app.js created, index.html created, project started, initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const div = document.createElement("div");
+const p = document.createElement("p");
+p.textContent = "Today, It is going to be Sunny";
+div.appendChild(p);
+
+div.style.display = "flex";
+div.style.width = "width";
+const button = document.createElement("button");
+const span = document.createElement("span");
